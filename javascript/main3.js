c16db18f2eac1dcb7ded66e4acc88ee1a1637f0b
Abdelrahmans00 <123298652+Abdelrahmans00@users.noreply.github.com>
2025-02-21 20:13:44 +0200

    main3.js

--- javascript/main3.js
+++ javascript/main3.js
@@ -13,17 +13,21 @@ async function getTopActors()
 getTopActors(); 
 
 
-function displayTopActors()
-{
-    let box = `` ; 
-    for(let i =0 ; i< topActors.length ; i++)
-    {
-        box+= ` <div class="col-md-3 mt-3">
-                    <img class="w-100" src=${baseUrlActors + topActors[i].profile_path} alt="tv show">
-                    <h4 class="text-warning">${topActors[i].name}</h4>
-                    <h3 class="text-danger">${topActors[i].known_for_department}</h3>
-                    <p class="text-muted">${topActors[i].popularity}</p>
-                </div>`
+function displayTopActors() {
+    let box = ``; 
+    for (let i = 0; i < topActors.length; i++) {
+        let popularity = Math.floor(topActors[i].popularity); 
+        let imageUrl = topActors[i].profile_path 
+            ? baseUrlActors + topActors[i].profile_path 
+            : "https://via.placeholder.com/500"; 
+
+        box += ` 
+            <div class="col-md-3 mt-3">
+                <img class="w-100" src="${imageUrl}" alt="${topActors[i].name}">
+                <h4 class="text-warning">${topActors[i].name}</h4>
+                <h3 class="text-danger">${topActors[i].known_for_department}</h3>
+                <p class="text-muted">${popularity} &#9733;</p>  <!-- Unicode Star -->
+            </div>`;
     }
-    document.getElementById('posts').innerHTML = box ; 
+    document.getElementById('posts').innerHTML = box;
 }
